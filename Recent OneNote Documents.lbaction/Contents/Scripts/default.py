521ec12634318f16ca5129da2f1adad05e2872da
Nicholas Riley <com-github@sabi.net>
2017-01-16 08:00:35 -0500

    Sort OneNote documents by most recent use.

--- Recent OneNote Documents.lbaction/Contents/Scripts/default.py
+++ Recent OneNote Documents.lbaction/Contents/Scripts/default.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json, os, sqlite3, urllib
+import json, operator, os, sqlite3, urllib
 
 APP_NAME = 'OneNote'
 APP_BUNDLE_ID = 'com.microsoft.onenote.mac'
@@ -10,9 +10,8 @@ mruservicecache_path = os.path.expanduser(
     '~/Library/Containers/%s/Data/Library/Application Support/Microsoft/AppData/Office/15.0/MruServiceCache'
     % APP_BUNDLE_ID)
 
-items = []
 
-seen_urls = set()
+documents = []
 
 for cache_path in os.listdir(mruservicecache_path):
     # XXX localization?
@@ -21,16 +20,22 @@ for cache_path in os.listdir(mruservicecache_path):
         continue
 
     # XXX exception handling
-    documents = json.load(file(documents_path))
-    for document in documents:
-        url = document['DocumentUrl']
-        if url in seen_urls:
-            continue
-        seen_urls.add(url)
-        items.append(dict(title=document['FileName'],
-                          subtitle=document['Path'],
-                          url='%s:%s' % (APP_SCHEMA,
-                                         urllib.quote(document['DocumentUrl'], safe=':/')),
-                          icon=APP_BUNDLE_ID))
+    documents += json.load(file(documents_path))
+
+items = []
+seen_urls = set()
+
+documents.sort(key=operator.itemgetter('Timestamp'), reverse=True)
+
+for document in documents:
+    url = document['DocumentUrl']
+    if url in seen_urls:
+        continue
+    seen_urls.add(url)
+    items.append(dict(title=document['FileName'],
+                      subtitle=document['Path'],
+                      url='%s:%s' % (APP_SCHEMA,
+                                     urllib.quote(document['DocumentUrl'], safe=':/')),
+                      icon=APP_BUNDLE_ID))
 
 print json.dumps(items)
