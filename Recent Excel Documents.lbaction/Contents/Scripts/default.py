098e82f1d5a59ef556ed9fc1d46da794167157ec
Nicholas Riley <com-github@sabi.net>
2017-02-05 16:12:16 -0500

    Merge local and cloud documents for Excel.

--- Recent Excel Documents.lbaction/Contents/Scripts/default.py
+++ Recent Excel Documents.lbaction/Contents/Scripts/default.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
 
-import json, os, sqlite3
+import json, operator
+import mruservice, mruuserdata
 
-conn = sqlite3.connect(os.path.expanduser('~/Library/Group Containers/UBF8T346G9.Office/MicrosoftRegistrationDB.reg'))
+APP_NAME = 'Excel'
+APP_BUNDLE_ID = 'com.microsoft.Excel'
+APP_URL_PREFIX = 'ms-excel:ofe|u|'
+EXTENSION_TO_ICON_NAME = dict(
+    slk='XLS8', dif='XLS8', ods='ODS', xls='XLS8', xlsx='XLSX', xltx='XLTX', xlsm='XLSM',
+    xltm='XLTM', xlsb='XLSB', xlam='XLAM', xlw='XLW8', xla='XLA8', xlb='XLB8', xlt='XLT',
+    xld='XLD5', xlm='XLM4', xll='XLL', csv='CSV', txt='TEXT', xml='XMLS', tlb='OTLB', _='TEXT')
 
-SQL = """select url.value URL from HKEY_CURRENT_USER_values url
-join HKEY_CURRENT_USER_values ts using (node_id)
-join HKEY_CURRENT_USER_values filename using (node_id)
-join HKEY_CURRENT_USER uuid using (node_id)
-join HKEY_CURRENT_USER documents on uuid.parent_id = documents.node_id and documents.name = 'Documents'
-join HKEY_CURRENT_USER local on documents.parent_id = local.node_id and local.name = 'Local'
-join HKEY_CURRENT_USER app on local.parent_id = app.node_id and app.name = ?
-join HKEY_CURRENT_USER user on app.parent_id = user.node_id
-join HKEY_CURRENT_USER mru on user.parent_id = mru.node_id and mru.name = 'MruUserData'
-join HKEY_CURRENT_USER common on mru.parent_id = common.node_id and common.name = 'Common'
-join HKEY_CURRENT_USER version on common.parent_id = version.node_id
-join HKEY_CURRENT_USER office on version.parent_id = office.node_id and office.name = 'Office'
-join HKEY_CURRENT_USER microsoft on office.parent_id = microsoft.node_id and microsoft.name = 'Microsoft'
-join HKEY_CURRENT_USER software on microsoft.parent_id = software.node_id and software.name = 'Software'
-where software.parent_id = -1
-and url.name = 'DocumentUrl' and ts.name = 'Timestamp' and filename.name = 'FileName'
-order by ts.value desc
-"""
+items = mruuserdata.items_for_app(APP_NAME)
+items += mruservice.items_for_app(APP_NAME, APP_BUNDLE_ID, APP_URL_PREFIX, EXTENSION_TO_ICON_NAME)
 
-items = [dict(path=path) for path, in conn.execute(SQL, ('Excel',)) if os.path.exists(path)]
+items.sort(key=operator.itemgetter('Timestamp'), reverse=True)
 
 print json.dumps(items)
